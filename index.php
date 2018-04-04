9bd00ce2bdb90ca6db8f18bd4baef5ce2ce24969
DESKTOP-US1F0G2\mjlee <adios0716@gmail.com>
2018-04-04 20:30:28 +0900

    메인페이지 작성

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Friends</title>
+    </head>
+    <body>
+        <h1>Friends</h1>
+        <ol>
+            <li>이철우</li>
+            <li>안병현</li>
+            <li>이헌준</li>
+        </ol>
+        <h2>환영합니다.</h2>
+        <article>
+            친구에 관한 홈페이지에 오실 걸 환엽합니다.
+        </article>
+        <footer>
+            일본 도쿄
+        </footer>
+
+    </body>
+</html>
