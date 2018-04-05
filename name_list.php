ccaa77efa912952ea4434bd9c786874b26c6bd5e
DESKTOP-US1F0G2\mjlee <adios0716@gmail.com>
2018-04-06 05:29:17 +0900

    데이터에서 name_list 테이블 가져오기 작성

--- name_list.php
+++ name_list.php
@@ -1,4 +1,27 @@
 <?php
+
+function fetchTable()
+{
+    $conn = mysqli_connect('localhost', 'root', '', 'friends');
+    if (mysqli_connect_errno($conn)) {
+        echo "접속오류 발생: " . mysqli_connect_error($conn);
+        exit;
+    }
+// 한글깨짐 방지를 위한 글자코드변경-mysql서버로 코드도 한글깨짐 방지를 위해 utf8
+    mysqli_query($conn, "set session character_set_connection=utf8;");
+    mysqli_query($conn, "set session character_set_results=utf8;");
+    mysqli_query($conn, "set session character_set_client=utf8;");
+    $sql = "SELECT * FROM name_list";
+    $result = mysqli_query($conn, $sql);
+    while ($row = mysqli_fetch_array($result)) {
+        echo "<tr>\n";
+        echo "<td>{$row['id']}</td>\n";
+        echo "<td>{$row['name']}</td>\n";
+        echo "<td>{$row['sex']}</td>\n";
+        echo "<td>{$row['age']}</td>\n";
+        echo "</tr>\n";
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -9,11 +32,15 @@
     </head>
     <body>
         <h1>Topic</h1>
-        <ol>
-            <li>이철우</li>
-            <li>안병현</li>
-            <li>이헌준</li>
-        </ol>
+        <table border="1">
+            <tr>
+                <th>ID</th>
+                <th>이름</th>
+                <th>성별</th>
+                <th>나이</th>
+            </tr>
+            <?php fetchTable(); ?>
+        </table>
         <h2>친구등록</h2>
         <form action="create_process_name_list.php" method="post">
             <p><label>이름 </label><input type="text" name="name" style="width:50px"/></p>
